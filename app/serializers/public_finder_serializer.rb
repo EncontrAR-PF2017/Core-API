7db54d861e7ebd7c51b45bdf328d7ceb1da794e5
lucasdelatorre <luksdlt92@gmail.com>
2017-10-13 22:44:27 -0300

    Fixing finder serializer

--- app/serializers/public_finder_serializer.rb
+++ app/serializers/public_finder_serializer.rb
@@ -1,3 +1,3 @@
 class PublicFinderSerializer < ActiveModel::Serializer
-	attributes :id, :email, :name, :lastname, :os, :alert_type, :latitude, :longitude, :created_at
+	attributes :id, :email, :name, :lastname, :os, :alert_type, :dni, :latitude, :longitude, :created_at
 end
