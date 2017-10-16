d85f7aaeda4bd10d87639624a59f14d52d72d997
lucasdelatorre <luksdlt92@gmail.com>
2017-10-16 01:56:04 -0300

    Sending images to SNS

--- app/workers/send_alert_worker.rb
+++ app/workers/send_alert_worker.rb
@@ -5,11 +5,10 @@ class SendAlertWorker
 	def perform(alert_id, campaign_id)
 		alert = Alert.find_by_id(alert_id)
 		campaign = Campaign.find_by_id(campaign_id)
-		campaign.images
 		return unless alert.present? && campaign.present?
 
 		finders = GeoSearchHelper.search_finders_by_zone(alert.zone)
-		message_content = { message: campaign }
+		message_content = { message: campaign, images: campaign.images }
 		finders.each do |finder| 
 			Wor::Push::Notifications::Aws.send_message(finder, message_content)
 		end
