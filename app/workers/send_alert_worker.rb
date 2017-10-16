e2aa47c624c5190bcb807d35b976e6bac1bc2803
lucasdelatorre <luksdlt92@gmail.com>
2017-10-16 01:20:58 -0300

    Trying another key value config

--- app/workers/send_alert_worker.rb
+++ app/workers/send_alert_worker.rb
@@ -8,7 +8,7 @@ class SendAlertWorker
 		return unless alert.present? && campaign.present?
 
 		finders = GeoSearchHelper.search_finders_by_zone(alert.zone)
-		message_content = { message: campaign }
+		message_content = { data: campaign }
 		finders.each do |finder| 
 			Wor::Push::Notifications::Aws.send_message(finder, message_content)
 		end
