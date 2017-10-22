31b8aae01663d4f8328717194286724fd09014fd
lucasdelatorre <luksdlt92@gmail.com>
2017-10-22 01:13:00 -0300

    Fixing the campaign expire worker

--- app/workers/expire_campaign_worker.rb
+++ app/workers/expire_campaign_worker.rb
@@ -5,11 +5,10 @@ class ExpireCampaignWorker
 
   def perform
     campaigns = Campaign.where(status: 'actived')
-    campaigns.each do |campaign|
+    campaigns.select{ |c| c.expire_date.present? }.each do |campaign|
     	if campaign.expire_date < Date.current()
     		campaign.status = 'expired'
     		campaign.save
-
     		puts 'The campaign ${campaign.title} was expired'
     	end
     end
