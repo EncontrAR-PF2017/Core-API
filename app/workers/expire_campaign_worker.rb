2e538338b1bffe8f10063f2a1a4b982460e6de89
lucasdelatorre <luksdlt92@gmail.com>
2017-10-22 01:07:32 -0300

    Implementing expire campaign worker

--- app/workers/expire_campaign_worker.rb
+++ app/workers/expire_campaign_worker.rb
@@ -4,6 +4,14 @@ class ExpireCampaignWorker
   include Sidekiq::Worker
 
   def perform
-    puts 'Hello world'
+    campaigns = Campaign.where(status: 'actived')
+    campaigns.each do |campaign|
+    	if campaign.expire_date < Date.current()
+    		campaign.status = 'expired'
+    		campaign.save
+
+    		puts 'The campaign ${campaign.title} was expired'
+    	end
+    end
   end
 end
