be11f2efe22abd07b184110efcc144b3339ee4c8
lucasdelatorre <luksdlt92@gmail.com>
2017-10-22 01:23:36 -0300

    Implementing alert expiration worker

--- app/workers/expire_alert_worker.rb
+++ app/workers/expire_alert_worker.rb
@@ -4,6 +4,13 @@ class ExpireAlertWorker
   include Sidekiq::Worker
 
   def perform
-    puts 'Hello world'
+    alerts = Alert.where(status: 'enabled')
+    alerts.select{ |a| a.expire_date.present? }.each do |alert|
+    	if alert.expire_date < Date.current()
+    		alert.status = 'expired'
+    		alert.save
+    		puts "The alert #{alert.id} has expired"
+    	end
+    end
   end
 end
