21a6cb3eefd8546689ae2bd5c09db2f3c36f710c
lucasdelatorre <luksdlt92@gmail.com>
2017-11-11 21:17:49 -0300

    Configuring enviroment variables

--- config/initializers/wor_push_notifications_aws.rb
+++ config/initializers/wor_push_notifications_aws.rb
@@ -1,8 +1,6 @@
 Wor::Push::Notifications::Aws.configure do |config|
-  config.device_types = [:ios, :android]       		# optional
+  config.device_types = [:android]       		# optional
   config.table_name = 'finders'                 		# optional
-  config.aws_region = 'us-west-2'
-  config.aws_android_arn = 'arn:aws:sns:us-west-2:200783692416:app/GCM/Encontrar-Firebase'     # mandatory field if you choose to use Android devices
-  config.aws_ios_arn = 'some:ios:arn'             # mandatory field if you choose to use iOS devices
-  config.aws_ios_sandbox = true                   # mandatory field if you choose to use iOS devices
+  config.aws_region = ENV['AWS-REGION'] ? ENV['AWS-REGION'] : 'us-west-2'
+  config.aws_android_arn = ENV['AWS-ANDROID-ARN'] ? ENV['AWS-ANDROID-ARN'] : ''     # mandatory field if you choose to use Android devices
 end
